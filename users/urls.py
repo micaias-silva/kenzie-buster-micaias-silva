a7e1203b4c32076099723ed45dce1a159a9c2ff7
Micaías Silva <micaiasilvaop@gmail.com>
2022-12-15 13:35:03 -0300

    feat: login views and token refresh

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 from .views import *
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-urlpatterns = [path("users/", UsersView.as_view())]
+urlpatterns = [
+    path("users/", UsersView.as_view()),
+    path("users/login/", TokenObtainPairView.as_view()),
+    path("users/login/refresh", TokenRefreshView.as_view()),
+]
